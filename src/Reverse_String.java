3ced68c0458078da0c2eb3b8ca0508530d546cd3
Adhinath9987 <automation.adhinath@gmail.com>
2025-06-05 00:19:36 +0530

    Test Reverse project

---
+++ src/Reverse_String.java
@@ -0,0 +1,15 @@
+public class Reverse_String {
+    public static void main(String[] args) {
+
+        String s  = "Samiksha";
+        String r = "";
+        char ch ;
+
+        for (int i = 0 ; i < s.length() ; i++)
+        {
+            ch = s.charAt(i);
+            r = ch + r ;
+        }
+        System.out.println(r);
+    }
+}
